14dd22653d24e2fc6051a9c62cc07fa85f55355f
stevendreed <steven.daniel.reed@gmail.com>
2023-12-04 20:00:54 -0800

    created setTime function to decrement a global timer for the quiz

--- js/questions.js
+++ js/questions.js
@@ -1,6 +1,8 @@
 /*
   questions.js
 */
+const timeCounter = document.getElementsByClassName('timer');
+var timeRemaining = 10; // 100 centiseconds => 10 seconds
 
 // create ascii duck
 const duckieArt = function(faceValue)
@@ -8,7 +10,33 @@ const duckieArt = function(faceValue)
     return `    _
  __(${faceValue})<
 /____)`
+} 
+
+console.log(duckieArt('^') + '  quack quack quack quack quack');
+
+// time countdown
+
+const setTime = function()
+{
+    let tInter = setInterval(
+        () =>
+        {
+            console.log(`time: ${timeRemaining / 10}`);
+            timeRemaining--;
+            if (timeRemaining < 0)
+            {
+                clearInterval(tInter);
+                console.log('zing, timer is up!');
+            }
+        }, 100
+    )
 }
 
-console.log(duckieArt('^'));
+// eventListener to begin quiz game 
+// 
+// document.addEventListener('click', () =>
+// {
 
+// })
+
+setTime();
